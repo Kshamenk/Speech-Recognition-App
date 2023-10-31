c884e702abf62fe0eba2424910cea06bb2302678
Kshamenk Benitez <122121634+Kshamenk@users.noreply.github.com>
2023-10-31 17:35:42 -0300

    More structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import micoff from "./Images/microphone-off.webp";
 import micon from "./Images/microphone.webp";
 import { IconButton } from "@mui/material";
+import { NeonButton } from "./Components/StyledComponents/StyledComponents";
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <img className="mic-icon" src={micoff} alt="microfono" />
         </IconButton>
       </div>
+      <NeonButton>To Do</NeonButton>
     </div>
   );
 }
